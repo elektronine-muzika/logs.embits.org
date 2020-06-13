f233cf210d94e23289bf784c1a8090ba17408cd9
martynaz <martynazzz@icloud.com>
2020-06-13 19:17:00 +0300

    add sentry

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import io.sentry.Sentry;
 import play.mvc.*;
 
 /**
@@ -15,7 +16,32 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
+        Sentry.init("https://4dff8390f1a2491cbfd09e3734322b21@o312835.ingest.sentry.io/5275496");
+        //this.logWithStaticAPI();
+
         return ok(views.html.index.render());
+    }
+
+    /**
+     * An example method that throws an exception.
+     */
+    void unsafeMethod() {
+        throw new UnsupportedOperationException("You shouldn't call this!!!");
+    }
+
+    /**
+     * Examples using the (recommended) static API.
+     */
+    void logWithStaticAPI() {
+        Sentry.capture("HomeController - index capture");
+
+        try {
+            unsafeMethod();
+        } catch (Exception e) {
+            // This sends an exception event to Sentry using the statically stored instance
+            // that was created in the ``main`` method.
+            Sentry.capture(e);
+        }
     }
 
 }
